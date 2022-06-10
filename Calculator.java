5b877d339fe0935036c3ef087ab2ced0eb9b10d4
akshay rathod <30733682+akshayrathod8@users.noreply.github.com>
2022-06-10 21:45:03 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+package study.basics.maths;
+
+public class Calculator {
+	
+	public static int calculate(int num1,int num2,char operator)
+	{
+		int ans;
+		switch(operator)
+		{
+		case '+':
+			ans=num1+num2;
+			return ans;
+			
+		case '-':
+			ans=num2-num1;
+			return ans;
+		case '*':
+			ans=num1*num2;
+			return ans;
+		case '/':
+			ans=num2/num1;
+			return ans;
+		default:
+			return -1;
+				
+		}
+	}
+
+}
